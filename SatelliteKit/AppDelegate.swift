5bc8381275c20945b4a98e8f1e0d682a678da65e
Alvin Alleyne <mocojumb@gmail.com>
2020-09-09 20:36:09 -0500

    Initial Commit

---
+++ SatelliteKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SatelliteKit
+//
+//  Created by Alvin Alleyne on 9/9/20.
+//  Copyright © 2020 AlvinAlleyne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
