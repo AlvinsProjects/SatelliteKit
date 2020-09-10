3a8f43f5f878b2b716955872539605b487104640
Alvin Alleyne <mocojumb@gmail.com>
2020-09-09 22:03:19 -0500

    New Project with all of the files imported fromSatelliteKit.  Three errors that needs to be fixed.  This is the original commit.

---
+++ SatelliteKit/SatelliteKit.swift
@@ -0,0 +1,26 @@
+/*╔══════════════════════════════════════════════════════════════════════════════════════════════════╗
+  ║ SatelliteKit.swift                                                                  SatelliteKit ║
+  ║ Created by Gavin Eadie on Aug05/17 ... Copyright 2016-20 Ramsay Consulting. All rights reserved. ║
+  ╚══════════════════════════════════════════════════════════════════════════════════════════════════╝*/
+
+import Foundation
+
+public struct SatelliteKit {
+
+    var staticVersion = "~~AppBuildDate~~ ~~AppBuildTime~~"
+
+    public static var version: String {
+        guard let satelliteKitBundle = Bundle(identifier: "com.ramsaycons.SatelliteKit"),
+              let plistDictionary = satelliteKitBundle.infoDictionary else {
+                return "No 'SatelliteKit' Info.plist"
+        }
+
+        return String(format: "%@ v%@ (#%@) [%@ @ %@]",
+                      plistDictionary["CFBundleName"] as? String ?? "Library",
+                      plistDictionary["CFBundleShortVersionString"] as? String ?? "v0.0",
+                      plistDictionary["CFBundleVersion"] as? String  ?? "0",
+                      plistDictionary["AppBuildDate"] as? String ?? "BuildDate",
+                      plistDictionary["AppBuildTime"] as? String ?? "BuildTime")
+    }
+
+}
